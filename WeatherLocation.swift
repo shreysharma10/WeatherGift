884047bb5c118cb9eb543b233b1b6a28d12f2bae
Shrey Sharma <sharmajk@bc.edu>
2022-03-15 20:59:41 -0400

    with .gitignore

---
+++ WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Shrey Sharma on 3/2/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
