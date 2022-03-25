318af16b661cbcd9e12f929d5130cde5ab1e88ce
Shrey Sharma <sharmajk@bc.edu>
2022-03-25 15:56:47 -0400

    finished week 7

---
+++ WeatherGift/LocationDetailViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LocationDetailViewController.swift
+//  WeatherGift
+//
+//  Created by Shrey Sharma on 3/25/22.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var weatherDetail: WeatherDetail!
+    var locationIndex = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUserInteface()
+        
+    }
+    
+    
+    
+    
+    
+    func updateUserInteface() {
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        let weatherLocation = pageViewController.weatherLocations[locationIndex]
+        weatherDetail = WeatherDetail(name: weatherLocation.name, latitude: weatherLocation.latitude, longitude: weatherLocation.longitude)
+        
+        pageControl.numberOfPages = pageViewController.weatherLocations.count
+        pageControl.currentPage = locationIndex
+        weatherDetail.getData {
+            DispatchQueue.main.async {
+                self.dateLabel.text = self.weatherDetail.timezone
+                self.placeLabel.text = self.weatherDetail.name
+                self.temperatureLabel.text = "\(self.weatherDetail.temperature)°"
+                self.summaryLabel.text = self.weatherDetail.summary
+            }
+        }
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! LocationListViewController
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        destination.weatherLocations = pageViewController.weatherLocations
+    }
+    
+    @IBAction func unwindFromLocationListViewController(segue: UIStoryboardSegue) {
+        let source = segue.source as! LocationListViewController
+        locationIndex = source.selectedLocationIndex
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        pageViewController.weatherLocations = source.weatherLocations
+        pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: locationIndex)], direction: .forward, animated: false, completion: nil)
+    }
+    
+    @IBAction func pageControlTapped(_ sender: UIPageControl) {
+        
+        var direction: UIPageViewController.NavigationDirection = .forward
+        if sender.currentPage < locationIndex {
+            direction = .reverse
+        }
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
+    }
+    
+    
+}
--- WeatherGift/LocationListViewController.swift
+++ WeatherGift/LocationListViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import GooglePlaces
+
 
 class LocationListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -13,21 +15,32 @@ class LocationListViewController: UIViewController {
     @IBOutlet weak var editBarButton: UIToolbar!
     
     
-    
-    
-    
     var weatherLocations: [WeatherLocation] = []
+    var selectedLocationIndex = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var weatherLocation = WeatherLocation(name: "Chestnut Hill, MA", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Lilongwe, Malawi", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        weatherLocation = WeatherLocation(name: "Buenos Aires, Argentina", latitude: 0, longitude: 0)
-        weatherLocations.append(weatherLocation)
-        
         tableView.dataSource = self
         tableView.delegate = self
+    }
+    
+    
+    func saveLocations()
+    {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(weatherLocations) {
+            UserDefaults.standard.set(encoded, forKey: "weatherLocations")
+        } else {
+            print("Error saving encoded didn't work")
+        }
+            
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        selectedLocationIndex = tableView.indexPathForSelectedRow!.row
+        saveLocations()
     }
     
     @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
@@ -45,7 +58,21 @@ class LocationListViewController: UIViewController {
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
+        let autocompleteController = GMSAutocompleteViewController()
+            autocompleteController.delegate = self
+
+//            // Specify the place data types to return.
+//            let fields: GMSPlaceField = GMSPlaceField(rawValue: UInt(GMSPlaceField.name.rawValue) |
+//              UInt(GMSPlaceField.placeID.rawValue))!
+//            autocompleteController.placeFields = fields
+//
+//            // Specify a filter.
+//            let filter = GMSAutocompleteFilter()
+//            filter.type = .address
+//            autocompleteController.autocompleteFilter = filter
+
+            // Display the autocomplete view controller.
+            present(autocompleteController, animated: true, completion: nil)
     }
     
     
@@ -61,6 +88,7 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = weatherLocations[indexPath.row].name
+        cell.detailTextLabel?.text = "Lat:\(weatherLocations[indexPath.row].latitude), Long:\(weatherLocations[indexPath.row].longitude)"
         return cell
     }
     
@@ -80,4 +108,30 @@ extension LocationListViewController: UITableViewDataSource, UITableViewDelegate
     }
     
     
+}
+
+extension LocationListViewController: GMSAutocompleteViewControllerDelegate {
+
+  // Handle the user's selection.
+  func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+    print("Place name: \(place.name)")
+    print("Place ID: \(place.placeID)")
+    print("Place attributions: \(place.attributions)")
+    
+    let newLocation = WeatherLocation(name: place.name ?? "unknown place", latitude: place.coordinate.latitude, longitude: place.coordinate.longitude)
+      weatherLocations.append(newLocation)
+      tableView.reloadData()
+    dismiss(animated: true, completion: nil)
+  }
+
+  func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+    // TODO: handle the error.
+    print("Error: ", error.localizedDescription)
+  }
+
+  // User canceled the operation.
+  func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+    dismiss(animated: true, completion: nil)
+  }
+
 }
